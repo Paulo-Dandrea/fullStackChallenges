44c808d478e450eaca9faa6aaf859e8b9fef0c4c
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-05-12 17:03:17 -0300

    ajustes ao exercicio

--- bloco4/exercises/4_1/exs-1-2-3-4-5-operadores.js
+++ bloco4/exercises/4_1/exs-1-2-3-4-5-operadores.js
@@ -1,5 +1,6 @@
 //  Atenção, este programa não roda no terminal do vscode, apenas no terminal externo. 
-
+// importante instalar primeiro por npm o modulo, digite desta forma abaixo no terminal:
+// $ npm install readline-sync 
 
 var readlineSync = require('readline-sync');
  
@@ -10,11 +11,26 @@ let b = readlineSync.question('Digite outro: ');
 a = parseInt(a);
 b = parseInt(b);
 
-let operadorString = ['somar','subtrair','multiplicar','dividir', 'saber se é par ou impar'];
+let operadorString = ['somar','subtrair','multiplicar','dividir', 'saber se a soma da par ou impar'];
 let resultado;
 let index = readlineSync.keyInSelect (operadorString, 'O que deseja fazer?');
 
 if (operadorString[0]){
     resultado = a + b;
     console.log('o resultado da sua soma é: ' + resultado) 
+} else if (operadorString[1]){
+    resultado = a - b;
+    console.log ('o resultado da subtração é ' + resultado);
+} else if (operadorString[2]){
+    resultado = a * b;
+    console.log ('o resultado da mutiplicação é: ' + resultado);
+} else if (operadorString[3]){
+    resultado = a / b;
+    console.log ('o resultado da divisão é: ' + resultado)
+} else if (operadorString[4]){
+    if (((a+b) % 2) === 0){
+        console.log ('a soma dá par');
+    }else{
+        console.log ('a soma da impar');
+    }
 }
