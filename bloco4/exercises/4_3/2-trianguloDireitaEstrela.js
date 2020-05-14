eeac7de54a5b2e9c43cdaeeae2f05386a2ba2b9d
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-05-14 19:08:30 -0300

    whatever

--- bloco4/exercises/4_3/2-trianguloDireitaEstrela.js
+++ bloco4/exercises/4_3/2-trianguloDireitaEstrela.js
@@ -5,14 +5,7 @@ for ( let y = 0 ; y < n ; y += 1) {
     for ( let x = 0 ; x <= y ; x += 1){
     process.stdout.write ('o');
     }
-
-
-    
-    process.stdout.write('\n');
+     process.stdout.write('\n');
 }
 
 
-// process.stdout.write("Hello, World");
-// process.stdout.clearLine();
-// process.stdout.cursorTo(0);
-// process.stdout.write("\n"); // end the line
