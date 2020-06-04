f0fbb07720205d8b3eb5884b4b22346d11d59f3d
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-06-04 11:07:11 -0300

    function that returns a value of loweredCase object parameter

---
+++ CodeWars/8-kyu/welcome-Object-Database.js
@@ -0,0 +1,28 @@
+function greet(language) {
+  let database = {
+    english: 'Welcome',
+    ip_address_invalid: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso',
+  };
+
+  language = language.toLowerCase();
+
+  return database[language];
+}
+let result;
+console.log(greet('Lithuanian'));
