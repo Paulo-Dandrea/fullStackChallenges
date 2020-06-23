1b90977d714b7ffecae902c284a7d4a7859b6069
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-06-23 20:21:01 -0300

    exercicios de Jest finalizados

---
+++ Bloco9/jest/2testesbonus.test.js
@@ -0,0 +1,49 @@
+const testebonus2 = require('./2testesbonus');
+
+const { reverseInt, vowels} = testebonus2;
+
+test('ReverseInt function exists', () => {
+  expect(reverseInt).toBeDefined();
+});
+
+test('ReverseInt handles 0 as an input', () => {
+  expect(reverseInt(0)).toEqual(0);
+});
+
+test('ReverseInt flips a positive number', () => {
+  expect(reverseInt(5)).toEqual(5);
+  expect(reverseInt(15)).toEqual(51);
+  expect(reverseInt(90)).toEqual(9);
+  expect(reverseInt(2359)).toEqual(9532);
+});
+
+test('ReverseInt flips a negative number', () => {
+  expect(reverseInt(-5)).toEqual(-5);
+  expect(reverseInt(-15)).toEqual(-51);
+  expect(reverseInt(-90)).toEqual(-9);
+  expect(reverseInt(-2359)).toEqual(-9532);
+});
+
+
+
+
+
+test('Vowels is a function', () => {
+  expect(typeof vowels).toEqual('function');
+});
+
+test('returns the number of vowels used', () => {
+  expect(vowels('aeiou')).toEqual(5);
+});
+
+test('returns the number of vowels used when they are capitalized', () => {
+  expect(vowels('AEIOU')).toEqual(5);
+});
+
+test('returns the number of vowels used', () => {
+  expect(vowels('abcdefghijklmnopqrstuvwxyz')).toEqual(5);
+});
+
+test('returns the number of vowels used', () => {
+  expect(vowels('bcdfghjkl')).toEqual(0);
+});
