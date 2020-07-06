6a2dfa74394222f09ab9d39c2b109e09a20aa678
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-07-06 11:37:18 -0300

    exercicios de fixacao, ainda nao muito claro como o jest.fn() poderia adicionar o retorno de um valor em um funcao de callback

---
+++ bloco10-AsyncJest/2.JestMock/service.test.js
@@ -0,0 +1,46 @@
+const service = require('./service');
+let { randomNumber, isDivisible } = require('./service');
+
+test("#randomRgbColor", () => {
+  // testando quantas vezes a função foi chamada e qual seu retorno
+  service.randomRgbColor = jest
+    .fn()
+    .mockReturnValue('default value')
+    .mockReturnValueOnce('first call')
+    .mockReturnValueOnce('second call');
+
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(0);
+
+  expect(service.randomRgbColor()).toBe("first call");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(1);
+
+  expect(service.randomRgbColor()).toBe("second call");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(2);
+
+  expect(service.randomRgbColor()).toBe("default value");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(3);
+});
+
+// test('randomNumber returns something', () => {
+//     randomNumber = jest
+//     .fn()
+//     .mockReturnValue(0)
+//     .mockReturnValueOnce(5)
+//     .mockReturnValueOnce(2);
+
+//     expect(randomNumber).toHaveBeenCalledTimes(0);
+
+//     expect(randomNumber()).toBe(5);
+//     expect(isDivisible(3)).toBe(false);
+
+//     expect(randomNumber).toHaveBeenCalledTimes(1);
+
+//     // expect(randomNumber()).toBe(2);
+//     // expect(isDivisible(3)).toBe(false);
+
+//     // expect(randomNumber).toHaveBeenCalledTimes(2);
+
+// });
+
+
+//Simule (mocke) dois valores para a função randomNumber() retornar e um terceiro valor default. Chame a função isDivisible() quatro vezes num mesmo teste e cheque que os retornos são conforme esperado.
