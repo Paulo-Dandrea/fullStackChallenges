a768f033f3b2c6d0fff3043e931beefa836f4041
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-07-17 18:55:13 -0300

    final versioon of the fixation exercises

--- bl13 - propTypes-LifeCycle-Router/13.1- Children e PropTypes/child-proptypes/src/PictureList.jsx
+++ bl13 - propTypes-LifeCycle-Router/13.1- Children e PropTypes/child-proptypes/src/PictureList.jsx
@@ -18,27 +18,7 @@ const PicturesList = (props) => {
     )
 }
 
-
-            {/* <Pictures
-              height={200}
-              src="https://www.publicdomainpictures.net/pictures/270000/velka/interior-bedroom.jpg"
-              alt="Logo da Trybe"
-              legenda="Lista feita na Trybe"
-            >
-              Lista children na Trybe
-            </Pictures> */}
-
-// Pictures.propTypes = {
-//   alt: PropTypes.string,
-//   height: PropTypes.number, 
-//   children: PropTypes.any.isRequired, 
-//   }
+Pictures.propTypes = {
+  children: PropTypes.array
+  }
 export default PicturesList;
-
-// Acrescente uma prop alt e garanta que ela seja uma string obrigatória;
-
-// Acrescente uma prop height e garanta que ela seja um number, e adicione um valor default para ela de 400;
-
-// Garanta que a prop children da imagem seja obrigatória;
-
-// Crie um elemento PicturesList que receba, na prop children, uma array de Pictures e as renderize dentro de uma lista. Faça a checagem de tipos de children. Esse exercício é para rever um pouco o conteúdo de aulas passadas também! O elemento precisará ocupar a posição de Pictures no componente App.
