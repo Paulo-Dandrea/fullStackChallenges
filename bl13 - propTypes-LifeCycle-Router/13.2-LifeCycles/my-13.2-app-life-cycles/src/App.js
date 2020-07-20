dfec4c452815cabd1c5d1db21c6cf10ce6863aec
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-07-20 16:29:41 -0300

    bad execution of the bonus exercise

--- bl13 - propTypes-LifeCycle-Router/13.2-LifeCycles/my-13.2-app-life-cycles/src/App.js
+++ bl13 - propTypes-LifeCycle-Router/13.2-LifeCycles/my-13.2-app-life-cycles/src/App.js
@@ -9,33 +9,58 @@ class App extends React.Component {
     this.state = {
       counter: 0,
       // message: "",
+      array:[]
     };
     this.handlerNextDogClick = this.handlerNextDogClick.bind(this);
+    this.handleOnChangeInput = this.handleOnChangeInput.bind(this);
   }
 
 
   componentDidMount() {
     this.setState({
       counter: 10,
+      message: localStorage.getItem(Object.keys(localStorage)[0]),
+      array: []
+
     })
 
+    // this.fetchDog()
+
+    // document.addEventListener('keydown', (e) => {
+    //   if(e.keyCode === 13) {
+    //     this.handlerNextDogClick()
+    //   }
+    // } )
+
+  }
+
+
+  fetchDog(){
     fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then(data => this.setState({message: data.message}))
   }
 
   handlerNextDogClick() {
-    fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => this.setState({message: data.message}))
+    this.fetchDog()
   }
 
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   if (nextState.message && nextState.message.includes("terrier")) {
-  //     return false;
-  //   }
-  //   return true;
-  // }
+
+
+
+  componentDidUpdate(){
+    if(this.state.message) alert(this.state.message.split('/')[4])
+
+    // localStorage.setItem('dog', this.state.message)
+    // localStorage.setItem("dogURL", JSON.stringify(this.state.array));
+  }
+
+  handleOnChangeInput(event){
+    this.setState({
+      dogname: event.target.value
+    })
+  }
+
 
   render() {
     return (
@@ -48,6 +73,12 @@ class App extends React.Component {
         <CounterDisplay value={this.state.counter} />
         <Image img={this.state.message}/>
         <button onClick={this.handlerNextDogClick}>Next Dog</button>
+
+
+        <label htmlFor='dogName'>Nomeie o Dog:</label>
+        <input name='dogName' onBlur={this.handleOnChangeInput} type='text'></input>
+        <button onClick={localStorage.setItem(this.state.dogname, this.state.message)}>Salvar Dog</button>
+        <p>{this.state.dogname}</p>
       </div>
     );
   }
