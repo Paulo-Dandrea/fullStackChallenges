272441ad2f1e817ddadcb2986cbe5d46d961a0b3
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-31 18:37:35 -0300

    some lookup exercises

--- bl25-Mongo-AggregationFramework/aggregation part1.js
+++ bl25-Mongo-AggregationFramework/aggregation part1.js
@@ -210,3 +210,290 @@ db.clients.aggregate([
     $project: { _id: 0 },
   },
 ]);
+
+// Grouping by times specific name made a transaction
+db.transactions.aggregate([
+  {
+    $group: {
+      _id: "$from",
+      transferiu: {
+        $sum: 1
+      }
+    }  
+  }
+  ])
+
+  // Sum the values that client transfered
+
+  db.transactions.aggregate([
+    {
+      $group: {
+        _id: "$from",
+        totalTransferedAmount: {
+          $sum: "$value"
+        },
+        timesUserMadeTransaction: {
+          $sum: 1
+        }
+      }  
+    }
+    ])
+
+// Now, just project client that the totalTransfered was less then 2000
+
+db.transactions.aggregate([
+  {
+    $group: {
+      _id: "$from",
+      totalTransferedAmount: {
+        $sum: "$value"
+      },
+      timesUserMadeTransaction: {
+        $sum: 1
+      }
+    }  
+  },
+  {
+    $match: {
+      totalTransferedAmount: {$lt: 2000}
+    }
+  }
+  ])
+
+// total de todas transacoes feitas e seus valores
+  db.transactions.aggregate([
+    {
+      $group: {
+        _id: null,
+        valorTotalDeTransacoes: {
+          $sum: "$value"
+        },
+        transacoesFeitas: {
+          $sum: 1
+        }
+      }  
+    },
+    {$project: {_id:0}}
+    ])
+
+// total de transações feitas por cada banco
+
+db.transactions.aggregate([
+  {
+    $group: {
+      _id: "$bank",
+      valorTotalDeTransacoes: {
+        $sum: "$value"
+      },
+      transacoesFeitas: {
+        $sum: 1
+      }
+    }  
+  },
+  ])
+
+  // mesmo agrupamento, só mostrando bancos com valores de transacao maiores que 3000
+  
+
+  db.transactions.aggregate([
+    {
+      $group: {
+        _id: "$bank",
+        valorTotalDeTransacoes: {
+          $sum: "$value"
+        },
+        transacoesFeitas: {
+          $sum: 1
+        }
+      }  
+    }, 
+    {
+      $match: {
+        valorTotalDeTransacoes: {$gt: 3000}
+      }
+    }
+    ])
+
+
+// exercises
+
+//Exercício 1: Utilizando o estágio $match, escreva uma agregação para retornar somente os clientes do sexo "MASCULINO".
+
+db.clientes.aggregate([
+  { $match: { sexo: "MASCULINO" } },
+  { $project: { sexo: 1 } },
+])[
+  //Exercício 2: Utilizando o estágio $match, escreva uma agregação para retornar somente os clientes do sexo "FEMININO" e com data de nascimento entre os anos de 1995 e 2005.
+  {
+    $match: {
+      sexo: "FEMININO",
+      dataNascimento: {
+        $gt: ISODate("1995"),
+        $lt: ISODate("2005"),
+      },
+    },
+  }
+][
+  //Exercício 3: Utilizando o estágio $match, escreva uma agregação para retornar somente os clientes do sexo "FEMININO" e com data de nascimento entre os anos de 1995 e 2005, limitando a quantidade de documentos retornados em 5.
+  ({
+    $match: {
+      sexo: "FEMININO",
+      dataNascimento: {
+        $gt: ISODate("1995"),
+        $lt: ISODate("2005"),
+      },
+    },
+  },
+  { $limit: 5 })
+][
+  //Exercício 4: Conte quantos clientes do estado SC existem na coleção. Retorne um documento em que o campo _id contenha a UF e outro campo com o total.
+
+  ({
+    $match: {
+      "endereco.uf": "SC",
+    },
+  },
+  {
+    $group: {
+      _id: "$endereco.uf",
+      SCdocs: {
+        $sum: 1,
+      },
+    },
+  })
+][
+  //Exercício 5: Agrupe os clientes por sexo. Retorne o total de clientes de cada sexo no campo total.
+  {
+    $group: {
+      _id: "$sexo",
+      totalDeClientesPorSexo: {
+        $sum: 1,
+      },
+    },
+  }
+][
+  //Exercício 6: Agrupe os clientes por sexo e uf. Retorne o total de clientes de cada sexo no campo total.
+  {
+    $group: {
+      _id: { sexo: "$sexo", UF: "$endereco.uf" },
+      quantosPorSexoNesteEstado: {
+        $sum: 1,
+      },
+    },
+  }
+][
+  //Exercício 7: Utilizando a mesma agregação do exercício anterior, adicione um estágio de projeção para modificar os documentos de saída, de forma que se pareçam com o documento a seguir (não se importe com a ordem dos campos):
+  //Copiar
+  //{
+  //  "estado": "SP",
+  //  "sexo": "MASCULINO",
+  //  "total": 100
+  //}
+
+  ({
+    $group: {
+      _id: { x: "$sexo", UF: "$endereco.uf" },
+      total: {
+        $sum: 1,
+      },
+    },
+  },
+  {
+    $project: {
+      sexo: "$_id.x",
+      uf: "$_id.UF",
+      _id: 0,
+    },
+  })
+][
+  //Exercício 8: Descubra quais são os 5 clientes que gastaram o maior valor.
+
+  ({
+    $lookup: {
+      from: "clientes",
+      localField: "clienteId",
+      foreignField: "clienteId",
+      as: "cliente",
+    },
+  },
+  {
+    $sort: {
+      valorTotal: -1,
+    },
+  },
+  {
+    $unwind: {
+      path: "$cliente",
+    },
+  },
+  {
+    $project: {
+      nome: "$cliente.nome",
+      valorTotal: 1,
+      _id: 0,
+    },
+  },
+  { $limit: 5 })
+];
+
+//Exercício 9: Descubra quais são os 10 clientes que gastaram o maior valor no ano de 2019.
+[
+  {
+    $match: {
+      dataVenda: {
+        $gte: ISODate("2019-01-01"),
+        $lte: ISODate("2019-12-31"),
+      },
+    },
+  },
+  {
+    $lookup: {
+      from: "clientes",
+      localField: "clienteId",
+      foreignField: "clienteId",
+      as: "cliente",
+    },
+  },
+  {
+    $sort: {
+      valorTotal: -1,
+    },
+  },
+  {
+    $unwind: {
+      path: "$cliente",
+    },
+  },
+  {
+    $project: {
+      nome: "$cliente.nome",
+      valorTotal: 1,
+      _id: 0,
+    },
+  },
+  { $limit: 10 },
+];
+
+//Exercício 10: Descubra quantos clientes compraram mais de 5 vezes. Retorne um documento que contenha somente o campo clientes com o total de clientes.
+//Dica: O operador $count pode simplificar sua query.
+
+
+//Exercício 11: Descubra quantos clientes compraram menos de três vezes entre os meses de Janeiro de 2020 e Março de 2020.
+
+
+//Exercício 12: Descubra quais as três ufs que mais compraram no ano de 2020. Retorne os documentos no seguinte formato:
+//Copiar
+//{
+//  "totalVendas": 10,
+//  "uf": "SP"
+//}
+
+
+//Exercício 13: Encontre qual foi o total de vendas e a média de vendas de cada uf no ano de 2019. Ordene os resultados pelo nome da uf. Retorne os documentos no seguinte formato:
+//Copiar
+//{
+//  "_id": "MG",
+//  "mediaVendas": 9407.129225352113,
+//  "totalVendas": 142
+//}
+
