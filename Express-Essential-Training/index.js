25d357f76ca60c7686f3b015970290fc7dee49c6
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-11-16 08:47:37 -0300

    going to express debugging

--- Express-Essential-Training/index.js
+++ Express-Essential-Training/index.js
@@ -1,25 +1,76 @@
 const express = require("express");
+const path = require('path');
+const serveFavicon = require('serve-favicon');
 const data = require("./data/MOCK_DATA.json");
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
+// http://localhost:3000/tent.jpg
+app.use("/images", express.static("images"));
+// http://localhost:3000/images/rocket.jpg // Aqui nós específicamos que o caminho
+// inicial engloba images
+
+app.use(serveFavicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(express.urlencoded({extended: true})); // to parse everything to jason
+
 app.get("/", (req, res) => {
-  res.status(200).send(`a get request with / route on port ${PORT}`); // %s doesn't work here
+  res.status(200).json(data);
 });
 
-app.post("/newItem", (req, res) => {
-  res.send(`a post request with /newItem route on port ${PORT}`);
-});
+// JSON data
+// {"hello": "JSON IS cool"}
+// URLEncoded data
+// hello=URLEncoded+is+cool
 
-app.put("/item", (req, res) => {
-  res.send(`a put request with /item route on port ${PORT}`);
-});
+app.post('/newItem', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+})
 
-app.delete("/item", (req, res) => {
-  res.send(`a delete request with /item route on port ${PORT}`);
-});
+
+app.get(
+  "/item/:id",
+  (req, res, next) => {
+    // this is the middleware that pulls the data
+    console.log(req.params.id);
+    let userId = Number(req.params.id);
+    let userJson = data.filter((e) => e.id === userId);
+    // middleware that use the req object
+      console.log(`request from: ${req.originalUrl}`);
+      console.log(`request type: ${req.method}`);
+    // Everything above is a middleware
+    res.send(userJson); // You can only RESPONSE once per call of method
+    next();
+  },
+  (req, res) => {
+    console.log("did you get the right data?");
+  },
+);
+
+app
+  .route("/item")
+  .get((req, res) => {
+    // throw new Error();
+    res.send(`a get request with /item route on port ${PORT}`);
+    // res.end();//
+    // res.redirect('http://linkedin.com');
+    // res.download('images/rocket.jpg');
+  })
+  .put((req, res) => {
+    res.send(`a put request with /newItem route on port ${PORT}`);
+  })
+  .delete((req, res) => {
+    res.send(`a delete request with /item route on port ${PORT}`);
+  });
+
+  // Error handling function
+  app.use((err, _req, res, _next) => {
+    console.error(err);
+    res.status(500).send(`Red alert: ${err.stack}`);
+  })
 
 app.listen(PORT, () => {
   console.log("Your server is running on port %s ", PORT);
-  // console.log(data);
 });
