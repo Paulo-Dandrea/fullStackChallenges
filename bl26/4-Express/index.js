b2641ee204646f3d7d1fe4de008dfe12cf021f1b
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-11-17 10:02:02 -0300

    express post + fs.writeFile

--- bl26/4-Express/index.js
+++ bl26/4-Express/index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require("express");
+const simpsonsRouter = require('./routeSimpsons');
 
 const app = express();
 
@@ -6,21 +7,43 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/ping', (req, res) => {
-  res.send({"message": "pong"})
-})
-// 3 - Crie um endpoint que receba requisições do tipo POST no caminho /hello , contendo o JSON { "name": "<nome do usuário>" } e retorne um JSON { "message": "Hello, <nome do usuário>!" } 
-app.post('/hello', (req, res) => {
-  res.send({"message": `Hello, ${req.body.name}`})
-})
+app.get("/ping", (req, res) => {
+  res.send({ message: "pong" });
+});
+// 3 - Crie um endpoint que receba requisições do tipo POST no caminho /hello , contendo o JSON { "name": "<nome do usuário>" } e retorne um JSON { "message": "Hello, <nome do usuário>!" }
+
+// app.post('/hello', (req, res) => {
+//   res.send({"message": `Hello, ${req.body.name}`})
+// })
 
 // 4 - Crie um endpoint que receba requisições do tipo POST no caminho /hello , contendo o JSON { "name": "<nome do usuário>", "age": "<idade do usuário>" } . Caso o usuário tenha idade superior a 17 anos, retorne um JSON { "message": "Hello, <nome do usuário>!" } com o status code 200 . Caso contrário, retorne o JSON { "message": "Unauthorized"} com o status code 401 ;
+app.post("/hello", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
 
-// app.put()
+  if(age > 17) {
+    res.send({"message": `Hello, ${name}`})
+  } else {
+    res.status(401).send({"message": "Unauthorized"});
+  }
+});
 
 // 5 - Crie um endpoint que receba requisições do tipo PUT no caminho /users/:name/:age e retorne o JSON { "message": "Seu nome é <name> e você tem <age> anos de idade" } .
 
+app.put('/users/:name/:age', (req, res) => {
+  res.send({"message": `Seu nome é ${req.params.name} e você tem ${req.params.age} anos de idade`})
+})
 
+// 6 e 7 - Agora você vai criar uma API de listagem dos personagens da série Simpsons. Como fonte de dados, você vai utilizar um arquivo JSON. 
+// Tive que olhar o gabarito por que não tivemos preparação para estes exercícios e/ou o enunciado não está bem explicado.
+
+app.use('/simpsons', simpsonsRouter);
+
+app.use((err, _req, res, _next) => {
+  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`)
+})
+
+// Inicia o server
 app.listen(PORT, () => {
   console.log(`Server on port ${PORT} entered`);
-})
+});
---
+++ bl26/4-Express/routeSimpsons.js
@@ -0,0 +1,44 @@
+const fs = require("fs").promises;
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+
+const readSimpsonsFile = async () => {
+  const simpsonsData = await fs.readFile(
+    path.resolve(__dirname, ".", "simpsons.json"),
+    "utf-8",
+  );
+  return JSON.parse(simpsonsData);
+};
+
+router.get("/", async (req, res) => {
+  const data = await readSimpsonsFile();
+  res.status(200).send(data);
+});
+
+// 8 - Crie um endpoint do tipo GET na rota /simpsons/:id que deve retornar apenas o personagem com o id informado na URL da requisição.
+
+router.get('/:id', async (req, res) => {
+  const data = await readSimpsonsFile();
+  const charID = req.params.id;
+  const charDocument = data.find(e => e.id === charID);
+  try {
+  res.status(200).send(charDocument);
+  }catch(err) {
+    res.status(500).send(`Algo deu errado. Messagem: ${err.message}`)
+  }
+})
+
+// 9 - Crie um endpoint do tipo POST na rota /simpsons que será responsável por cadastrar novos personagens.
+
+const writeOnSimpsonsFile = async ({id, name}) => {
+  const oldData = await readSimpsonsFile(); // é um array
+  const newData = [...oldData, {id, name}]
+  fs.writeFile(path.resolve(__dirname, '.', 'simpsons.json'), JSON.stringify(newData));
+}
+
+router.post('/', (req, res) => {
+  writeOnSimpsonsFile(req.body)
+})
+
+module.exports = router;
