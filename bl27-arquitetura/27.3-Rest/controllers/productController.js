09e2b6e63aff2cca0363c2314755125010766218
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-11-25 19:43:39 -0300

    falta tratamento de erro

--- bl27-arquitetura/27.3-Rest/controllers/productController.js
+++ bl27-arquitetura/27.3-Rest/controllers/productController.js
@@ -7,14 +7,14 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   const products = await ProductModel.getAll();
 
-  res.json(products);
+  res.status(200).json(products);
 });
 // tirei o 'get-by-id' e deixei só '/'
 // o index já tá tomando conta do '/products'
 router.get('/:id', async (req, res, next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.json(product);
+  res.status(200).json(product);
 });
 
 // tirei /add-user e deixei só '/'
@@ -24,14 +24,14 @@ router.post('/', async (req, res) => {
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.json(newProduct);
+  res.status(201).json(newProduct);
 });
 // tirei 'delete-user/:id' e deixei só '/' -
 // o index já tá tomando conta do '/products'
 router.delete('/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.json(products);
+  res.status(204).end();
 });
 // tirei 'update-user/:id' e deixei só '/' -
 // troquei de post para put, já que é um update
@@ -41,7 +41,7 @@ router.put('/:id', async (req, res) => {
 
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.json(products);
+  res.status(204).end();
 });
 
 module.exports = router;
