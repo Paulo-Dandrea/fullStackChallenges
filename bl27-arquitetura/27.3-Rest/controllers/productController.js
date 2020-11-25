3e96007fe9ee4794d00863279aba746de8b8e4a3
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-11-25 19:30:41 -0300

    change the correct http verbs or methods, and the correct endpoints

--- bl27-arquitetura/27.3-Rest/controllers/productController.js
+++ bl27-arquitetura/27.3-Rest/controllers/productController.js
@@ -2,39 +2,46 @@ const express = require('express');
 const ProductModel = require('../models/productModel');
 
 const router = express.Router();
-
-router.get('/list-products', async (req, res, next) => {
+// tirei o 'list-products' e deixei só '/'
+// o index já tá tomando conta do '/products'
+router.get('/', async (req, res, next) => {
   const products = await ProductModel.getAll();
 
-  res.send(products);
+  res.json(products);
 });
-
-router.get('/get-by-id/:id', async (req, res, next) => {
+// tirei o 'get-by-id' e deixei só '/'
+// o index já tá tomando conta do '/products'
+router.get('/:id', async (req, res, next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.send(product);
+  res.json(product);
 });
 
-router.post('/add-user', async (req, res) => {
+// tirei /add-user e deixei só '/'
+// o index já tá tomando conta do '/products'
+router.post('/', async (req, res) => {
   const { name, brand } = req.body;
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  res.json(newProduct);
 });
-
-router.delete('/delete-user/:id', async (req, res) => {
+// tirei 'delete-user/:id' e deixei só '/' -
+// o index já tá tomando conta do '/products'
+router.delete('/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.send(products);
+  res.json(products);
 });
-
-router.post('/update-user/:id', async (req, res) => {
+// tirei 'update-user/:id' e deixei só '/' -
+// troquei de post para put, já que é um update
+// o index já tá tomando conta do '/products'
+router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.send(products);
+  res.json(products);
 });
 
 module.exports = router;
